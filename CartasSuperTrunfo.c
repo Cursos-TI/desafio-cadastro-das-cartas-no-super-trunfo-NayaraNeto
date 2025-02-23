3ff71630a039dd268de952651bbbdfd69546f108
NayaraNeto <nayaraneto85@gmail.com>
2025-02-23 19:31:50 +0000

    Criei novo printf PIB

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,5 +23,6 @@ int main(){// Aqui é a função principal, toda aplicação C começa pela fun�
     printf("Nome da cidade: %s \n", nome);// Imprime a variável nome (Nome da cidade) com especificador de strings, pulando uma linha \n).
     printf("População: %d \n", populacao);//Imprime a variável populacao (População) com especificação de um número inteiro decimal, pulando uma linha \n).
     printf("Área: %.2f km²\n", area);// Imprime a variável area (Área em Km²) que exibe a especificação do ponto flutuante com duas casas decimais e exibe em "km²".
+    printf("PIB: %.2f bilhões de reais \n", pib);// Imprime a variável pib (PIB) com especificação do ponto flutuante com duas casas decimais e exibe "bilhões de reais", pulando a linha \n).
 
 }
