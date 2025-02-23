4f95260dca3ff79e52e7fe4c267650113363b058
NayaraNeto <nayaraneto85@gmail.com>
2025-02-23 20:07:11 +0000

    Criei float area 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -33,6 +33,7 @@ int main(){// Aqui é a função principal, toda aplicação C começa pela fun�
     char codigo2[5] = "B02";
     char nome2[20] = "Curitiba";
     int populacao2 = 173718;
+    float area2 = 173718;
     
 
 }
