3f4b93333e96e157dc18121392e75e302036e4ec
NayaraNeto <nayaraneto85@gmail.com>
2025-02-23 19:11:20 +0000

    Crei dois char

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,5 +5,6 @@ int main(){// Aqui é a função principal, toda aplicação C começa pela fun�
     printf ("\nDesafio Super Trunfo\n"); //Nome do Desafio, pulando linha no ínicio e no final para seguir aos dados das cartas.
 
     char carta[20] = "Carta 1" // Aqui uma variável (carta 1), que pode armazenar letra ou símbolo ou nome utilizando até 20 caracteres.
+    char estado[20] = "Estado" //Aqui uma variável de único caractere ('A'), podendo armazenar como uma letra ou símbolo. 
 
 }
