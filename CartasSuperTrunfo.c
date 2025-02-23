407c6ba4292cd598763cc11d7b1c021fa96e2cdd
NayaraNeto <nayaraneto85@gmail.com>
2025-02-23 18:58:46 +0000

    Criei a função principal

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,1 +1,7 @@
 #include <stdio.h> //Aqui é incluido uma biblioteca padrão de entrada e saída do programa e necessária para usar a função printf.
+int main(){// Aqui é a função principal, toda aplicação C começa pela função main.
+    // Abaixo foi iniciado printf com o nome do desafio, e a sintaxe das variáveis da carta 1 e carta 2 do desafio supertrunfo.c, seguindo a impressão das variáveis.
+
+    printf ("\nDesafio Super Trunfo\n"); //Nome do Desafio, pulando linha no ínicio e no final para seguir aos dados das cartas.
+
+}
